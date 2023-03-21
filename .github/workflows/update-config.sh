c44bb269069385f97873a5b9af3d7a192894be41
Ian Purton <ian.purton@gmail.com>
2023-03-21 06:55:34 +0000

    fix: set registry.

--- .github/workflows/update-config.sh
+++ .github/workflows/update-config.sh
@@ -10,10 +10,5 @@ do
     echo "Name $CONFIG_NAME"
     echo "Hash $HASH"
     # Update entries i.e. hash-trace-server: sha256:c677b52b14375c03eaede5ccdf4d40ccc9b7e9ef0157fd4b0f45b0e431f0c76d
-    sed -i "0,/hash-$CONFIG_NAME/{s/hash-$CONFIG_NAME.*$/hash-$CONFIG_NAME: $HASH/}" ../../Pulumi.yaml 
-
-    echo "Trying: docker tag $i $i:$1"
-    docker tag $i $i:$1
-    echo "Trying: docker tpushag $i:$1"
-    docker push $1:$1
+    sed -i "0,/hash-$CONFIG_NAME/{s/hash-$CONFIG_NAME.*$/hash-$CONFIG_NAME: $HASH/}" ../../Pulumi.yaml
 done
